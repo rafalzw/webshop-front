9dc53f0a168c4b51006734ef70a2eb8c9432e656
Rafal Zw <zwierz.rafal@gmail.com>
2022-10-21 20:59:37 +0200

    Added Product component.

---
+++ src/components/Product.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { PopularProduct } from '../data';
+import { ShoppingCartOutlined, SearchOutlined, FavoriteBorderOutlined } from '@mui/icons-material';
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5f5f5;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: #fff;
+  position: absolute;
+`;
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  cursor: pointer;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: #eee;
+    transform: scale(1.1);
+  }
+`;
+
+interface ProductProps {
+  item: PopularProduct;
+}
+
+export const Product = ({ item }: ProductProps) => {
+  return (
+    <Container>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+        <Icon>
+          <SearchOutlined />
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
--- src/data.ts
+++ src/data.ts
@@ -11,6 +11,10 @@ export interface Categories {
   img: string;
   title: string;
 }
+export interface PopularProduct {
+  id: number;
+  img: string;
+}
 
 export const sliderItems: SliderItems[] = [
   {
@@ -51,5 +55,40 @@ export const categories: Categories[] = [
     id: 3,
     img: 'https://images.pexels.com/photos/293406/pexels-photo-293406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     title: 'OBUWIE',
+  },
+];
+
+export const popularProducts: PopularProduct[] = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1608680574102-3a81ac3c1c88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1593030103066-0093718efeb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1594938328870-9623159c8c99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+  },
+  {
+    id: 4,
+    img: 'https://images.unsplash.com/photo-1594938374182-a57061dac3df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+  },
+  {
+    id: 5,
+    img: 'https://static4.sklep.dastan.pl/pol_pl_Koszula-Tailored-Twist-Paisley-1783_1.jpg',
+  },
+  {
+    id: 6,
+    img: 'https://static1.sklep.dastan.pl/pol_pl_Koszula-Slim-Fit-Due-Leaf-1756_1.png',
+  },
+  {
+    id: 7,
+    img: 'https://static1.sklep.dastan.pl/pol_pl_Buty-401-Mokka-Brown-1302_1.webp',
+  },
+  {
+    id: 8,
+    img: 'https://www.recman.pl/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/5/9/5900320104851.jpg',
   },
 ];
