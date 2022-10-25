ea973d28c69d4510da3b084404c79a9a8563657a
Rafal Zw <zwierz.rafal@gmail.com>
2022-10-25 23:11:21 +0200

    Styled Product view.

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -4,15 +4,97 @@ import { Navbar } from '../components/Navbar';
 import { Announcement } from '../components/Announcement';
 import { Newsletter } from '../components/Newsletter';
 import { Footer } from '../components/Footer';
+import { Add, Remove } from '@mui/icons-material';
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Price = styled.span``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 50px;
+`;
+const Title = styled.h1`
+  font-weight: 400;
+`;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const Price = styled.span`
+  font-weight: 300;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  margin: 30px 0;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 300;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 50%;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  margin: 0 5px;
+  border-radius: 10px;
+  border: 1px solid #d3d3d3;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid #d3d3d3;
+  background-color: #fff;
+  cursor: pointer;
+  font-weight: 600;
+
+  &:hover {
+    background-color: #eee;
+  }
+`;
 
 export const Product = () => {
   return (
@@ -28,9 +110,38 @@ export const Product = () => {
           <Desc>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque blanditiis consequatur
             culpa, dolor eum labore laudantium, modi necessitatibus pariatur quibusdam quidem, vero
-            voluptatibus. Ab at in nesciunt praesentium, quos recusandae.
+            voluptatibus. Ab at in nesciunt praesentium, quos recusandae. Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit. Aliquam consectetur distinctio dolorum earum totam.
+            Atque cum doloribus et eum minus necessitatibus neque omnis praesentium, reiciendis
+            repellendus sapiente totam velit, voluptatum.
           </Desc>
           <Price>699,99 zł</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Kolor</FilterTitle>
+              <FilterColor color='blue' />
+              <FilterColor color='black' />
+              <FilterColor color='gray' />
+            </Filter>
+            <Filter>
+              <FilterTitle>Rozmiar</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>46/170</FilterSizeOption>
+                <FilterSizeOption>46/176</FilterSizeOption>
+                <FilterSizeOption>46/182</FilterSizeOption>
+                <FilterSizeOption>48/170</FilterSizeOption>
+                <FilterSizeOption>48/176</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>DODAJ DO KOSZYKA</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
