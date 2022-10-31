d94b4d984b12f26b004864a6b3e5521f90f22463
Rafal Zw <zwierz.rafal@gmail.com>
2022-10-31 19:23:01 +0100

    Styled Login and Register pages for mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Login } from './pages/Login';
 import { Cart } from './pages/Cart';
 
 function App() {
-  return <Home />;
+  return <Register />;
 }
 
 export default App;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   width: 100vw;
@@ -16,6 +17,7 @@ const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: #fff;
+  ${mobile({ width: '80%' })}
 `;
 const Title = styled.h1`
   font-size: 24px;
