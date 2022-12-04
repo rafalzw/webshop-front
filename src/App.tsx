8b3286411ba1f624cd44775e4df8c90957558678
Rafal Zw <zwierz.rafal@gmail.com>
2022-12-04 21:29:30 +0100

    Added routes.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,22 @@ import { Product } from './pages/Product';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { Cart } from './pages/Cart';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <Cart />;
+  const user = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={user ? <Navigate replace to='/' /> : <Register />} />
+        <Route path='/login' element={user ? <Navigate replace to='/' /> : <Login />} />
+        <Route path='/products/:category' element={<ProductList />} />
+        <Route path='/product/:id' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
