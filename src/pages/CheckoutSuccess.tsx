8da3641e9bf1fc99a565c8d60057bb1aac316c88
Rafal Zw <zwierz.rafal@gmail.com>
2023-01-08 21:44:58 +0100

    Added Stripe checkout session request and createOrder function.

--- src/pages/CheckoutSuccess.tsx
+++ src/pages/CheckoutSuccess.tsx
@@ -1,5 +1,41 @@
-import React from 'react-router-dom';
+import React, { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { url } from '../config/config';
 
 export const CheckoutSuccess = () => {
+  const location = useLocation();
+  const [orderId, setOrderId] = useState();
+
+  const createOrder = async (customer: any, amount: number, items: any) => {
+    const res = await axios.post(
+      `${url}/orders`,
+      {
+        address: {
+          street: customer.address.street,
+          postCode: customer.address.postal_code,
+          city: customer.address.city,
+          country: customer.address.country,
+        },
+        amount: amount / 100,
+        products: items.map((item: any) => {
+          return { productId: item.price.product.metadata.id, quantity: item.quantity };
+        }),
+      },
+      { method: 'post', withCredentials: true },
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(`${url}/stripe/success/${location.search}`);
+      const { status, customer, amount, items } = res.data;
+
+      if (status === 'complete') {
+        await createOrder(customer, amount, items);
+      }
+    })();
+  }, []);
+
   return <h1>Success</h1>;
 };
