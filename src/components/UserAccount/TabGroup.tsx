389d297704eea0e7ae84a339635338d8d59e7c69
Rafal Zw <zwierz.rafal@gmail.com>
2023-01-16 21:07:15 +0100

    Added TabGroup component.

---
+++ src/components/UserAccount/TabGroup.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  active: boolean;
+}
+
+const Tab = styled.button`
+  font-size: 20px;
+  padding: 10px 60px;
+  cursor: pointer;
+  opacity: 0.6;
+  background: white;
+  border: 0;
+  outline: 0;
+
+  ${(props: Props) =>
+    props.active &&
+    `
+    border-bottom: 2px solid black;
+    opacity: 1;
+  `}
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+const types = ['Edytuj dane', 'Historia Zamówień'];
+
+export const TabGroup = () => {
+  const [active, setActive] = useState(types[0]);
+  return (
+    <>
+      <ButtonGroup>
+        {types.map((type) => (
+          <Tab key={type} active={active === type} onClick={() => setActive(type)}>
+            {type}
+          </Tab>
+        ))}
+      </ButtonGroup>
+      <p />
+      <p> Komponent: {active} </p>
+    </>
+  );
+};
